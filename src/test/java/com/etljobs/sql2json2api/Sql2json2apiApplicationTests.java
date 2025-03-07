17b2b2b9756c01545391dbb90cfdc2be9638dcfa
Hachim Hassani <hachim.hassani@gmail.com>
2025-03-07 19:45:09 +0100

    phase1

--- src/test/java/com/etljobs/sql2json2api/Sql2json2apiApplicationTests.java
+++ src/test/java/com/etljobs/sql2json2api/Sql2json2apiApplicationTests.java
@@ -1,13 +1,25 @@
 package com.etljobs.sql2json2api;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class Sql2json2apiApplicationTests {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(jdbcTemplate);
 	}
-
+	
+	@Test
+	void testDatabaseConnection() {
+		Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+		assertNotNull(result);
+	}
 }
