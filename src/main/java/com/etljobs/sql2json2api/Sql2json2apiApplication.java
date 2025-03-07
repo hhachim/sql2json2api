b123e8a75186f89c903545b3a9fcf68da86cbbb5
Hachim Hassani <hachim.hassani@gmail.com>
2025-03-07 16:55:07 +0100

    first commit : spring boot empty project

---
+++ src/main/java/com/etljobs/sql2json2api/Sql2json2apiApplication.java
@@ -0,0 +1,13 @@
+package com.etljobs.sql2json2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sql2json2apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sql2json2apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etljobs/sql2json2api/Sql2json2apiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etljobs.sql2json2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sql2json2apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
